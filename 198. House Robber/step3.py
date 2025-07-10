fd3a594a1e9609d1d4c5398a8817ca40a0410274
kazuryu <41988990+Kazuryu0907@users.noreply.github.com>
2025-07-11 02:17:53 +0900

    step1, 2, 3

---
+++ 198. House Robber/step3.py
@@ -0,0 +1,10 @@
+class solution:
+    def rob(self, nums: list[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums, default=0)
+        dp = [0 for _ in range(len(nums))]
+        dp[0] = nums[0]
+        dp[1] = max(nums[:2])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        return dp[-1]
